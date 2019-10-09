a536b85b1af98eb27e402bc313c88e6336fea1a0
Saeid Darvish <saeid.dq@gmail.com>
2019-10-09 22:08:53 +0330

    create MainActivity

---
+++ app/src/main/java/ir/sdrv/mobilletsample/presentation/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package ir.sdrv.mobilletsample.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.sdrv.mobilletsample.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
